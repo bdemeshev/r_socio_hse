702d0aa5ef8329dee43588c403e8fc713cb325a5
Boris Demeshev <boris.demeshev@gmail.com>
2017-04-05 19:25:33 +0300

    ups 2

--- script_03.R
+++ script_03.R
@@ -21,3 +21,24 @@ opisanie
 # самая популярная функция на группированных табличках
 # число наблюдений в группе
 count(diamonds, color)
+
+# сгруппировали по цвету
+# для каждого цвета посчитать разницу цены и 
+# средней цены бриллиантов данного цвета
+diamonds2 <- diamonds %>% group_by(color) %>%
+  mutate(otklonenie = price - mean(price))
+
+glimpse(diamonds2)
+
+# отбор строк
+diamonds3 <- filter(diamonds2, otklonenie > 0)
+glimpse(diamonds3)
+
+opisanie
+
+# переименовать переменную
+diamonds4 <- rename(diamonds3, raznost = otklonenie)
+
+Захвати Галактику!
+  
+
